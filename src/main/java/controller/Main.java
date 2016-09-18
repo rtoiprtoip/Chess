fbdf849ba5418944053e889b15559e890a59768d
Piotr <piotrub@gmail.com>
2016-09-18 11:08:32 +0200

    formatting

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -16,42 +16,42 @@ import model.EnPassantException;
 
 public class Main {
 
-	private static final View view = new view.swing.SwingView();
-	private static Game model;
-	private static MoveHistory moveHistory;
+    private static final View view = new view.swing.SwingView();
+    private static Game model;
+    private static MoveHistory moveHistory;
 
-	public static void main(String[] args) {
-		model = new Game();
-		SettingsHandler settingsHandler = new SettingsHandler();
+    public static void main(String[] args) {
+        model = new Game();
+        SettingsHandler settingsHandler = new SettingsHandler();
 
-		Thread timeCounter = new Thread() {
-			@Override
-			public void run() {
-				synchronized (this) {
-					while (true) {
-						view.setTime("white", getModel().getTime("white"));
-						view.setTime("black", getModel().getTime("black"));
-						try {
-							wait(50);
-						} catch (InterruptedException e) {
-						}
-					}
-				}
-			}
-		};
-		timeCounter.setDaemon(true);
-		timeCounter.start();
+        Thread timeCounter = new Thread() {
+            @Override
+            public void run() {
+                synchronized (this) {
+                    while (true) {
+                        view.setTime("white", getModel().getTime("white"));
+                        view.setTime("black", getModel().getTime("black"));
+                        try {
+                            wait(50);
+                        } catch (InterruptedException e) {
+                        }
+                    }
+                }
+            }
+        };
+        timeCounter.setDaemon(true);
+        timeCounter.start();
 
-		view.addSettingsListeners(e -> getModel().setPaused(!getModel().isPaused()), settingsHandler, settingsHandler);
+        view.addSettingsListeners(e -> getModel().setPaused(!getModel().isPaused()), settingsHandler, settingsHandler);
 
-		view.addNewGameStarter(e -> {
+        view.addNewGameStarter(e -> {
             getModel().newGame();
             updateChessboard();
             moveHistory = new MoveHistory(model);
             getModel().startOrResume();
         });
 
-		view.addGameLoader(e -> {
+        view.addGameLoader(e -> {
             File inputFile = view.getFileToReadFrom();
             if (inputFile == null) {
                 System.err.println("Load cancelled");
@@ -59,7 +59,7 @@ public class Main {
             }
 
             try (FileInputStream fileInput = new FileInputStream(inputFile);
-                    ObjectInputStream objIn = new ObjectInputStream(fileInput)) {
+                 ObjectInputStream objIn = new ObjectInputStream(fileInput)) {
 
                 moveHistory = (MoveHistory) objIn.readObject();
                 model = moveHistory.pop();
@@ -71,9 +71,9 @@ public class Main {
             } catch (IOException | ClassNotFoundException i) {
                 i.printStackTrace();
             }
-		});
+        });
 
-		view.addGameSaver(e -> {
+        view.addGameSaver(e -> {
             File outputFile = view.getFileToSaveIn();
             if (outputFile == null) {
                 System.err.println("Save failed");
@@ -82,7 +82,7 @@ public class Main {
 
             System.err.println("Saving");
             try (FileOutputStream fileOut = new FileOutputStream(outputFile);
-                    ObjectOutputStream objOut = new ObjectOutputStream(fileOut)) {
+                 ObjectOutputStream objOut = new ObjectOutputStream(fileOut)) {
                 objOut.writeObject(moveHistory);
             } catch (IOException i) {
                 i.printStackTrace();
@@ -90,7 +90,7 @@ public class Main {
 
         });
 
-		view.addLogSaver(e -> {
+        view.addLogSaver(e -> {
             File outputFile = view.getFileToSaveIn();
             if (outputFile == null) {
                 System.err.println("Save failed");
@@ -105,106 +105,106 @@ public class Main {
             }
         });
 
-		view.addPauseListener(e -> getModel().setPaused(!getModel().isPaused()));
+        view.addPauseListener(e -> getModel().setPaused(!getModel().isPaused()));
 
-		view.addEndGameListener(e -> {
+        view.addEndGameListener(e -> {
             getModel().endGame();
             view.clearGUI();
         });
 
-		view.addLegalStuffDisplayer(e -> getModel().setPaused(!getModel().isPaused()));
+        view.addLegalStuffDisplayer(e -> getModel().setPaused(!getModel().isPaused()));
 
-		view.addRevertMoveListener(e -> {
+        view.addRevertMoveListener(e -> {
             model = moveHistory.pop();
             updateChessboard();
             model.startOrResume();
         });
 
-		view.addMoveHandler((moveFrom, moveTo) -> new Thread(() -> {
-try {
-boolean moveAuthorized = getModel().isThisValidMove(moveFrom, moveTo);
-if (moveAuthorized) {
-getModel().move(moveFrom, moveTo);
-view.move(moveFrom, moveTo);
+        view.addMoveHandler((moveFrom, moveTo) -> new Thread(() -> {
+            try {
+                boolean moveAuthorized = getModel().isThisValidMove(moveFrom, moveTo);
+                if (moveAuthorized) {
+                    getModel().move(moveFrom, moveTo);
+                    view.move(moveFrom, moveTo);
 
-moveHistory.push(model, moveFrom, moveTo);
-System.err.println(moveFrom + "-" + moveTo);
+                    moveHistory.push(model, moveFrom, moveTo);
+                    System.err.println(moveFrom + "-" + moveTo);
 
-}
-} catch (PromotionException e) {
-String color = getModel().getWhoseMove().toString();
-String promotionChoice = view.getPromotionChoice(color);
-getModel().promote(moveFrom, moveTo, promotionChoice);
-promotionChoice = (color + "_" + promotionChoice).toLowerCase();
-view.promote(moveFrom, moveTo, promotionChoice);
+                }
+            } catch (PromotionException e) {
+                String color = getModel().getWhoseMove().toString();
+                String promotionChoice = view.getPromotionChoice(color);
+                getModel().promote(moveFrom, moveTo, promotionChoice);
+                promotionChoice = (color + "_" + promotionChoice).toLowerCase();
+                view.promote(moveFrom, moveTo, promotionChoice);
 
-moveHistory.push(model, moveFrom, moveTo, promotionChoice);
-System.err.println(moveFrom + "-" + moveTo);
+                moveHistory.push(model, moveFrom, moveTo, promotionChoice);
+                System.err.println(moveFrom + "-" + moveTo);
 
-} catch (CastlingException e) {
-getModel().castle(moveFrom, moveTo);
-view.castle(moveFrom, moveTo);
+            } catch (CastlingException e) {
+                getModel().castle(moveFrom, moveTo);
+                view.castle(moveFrom, moveTo);
 
-moveHistory.push(model, moveFrom, moveTo);
-System.err.println(moveFrom + "-" + moveTo);
+                moveHistory.push(model, moveFrom, moveTo);
+                System.err.println(moveFrom + "-" + moveTo);
 
-} catch (EnPassantException e) {
-getModel().enPassant(moveFrom, moveTo);
-view.enPassant(moveFrom, moveTo);
+            } catch (EnPassantException e) {
+                getModel().enPassant(moveFrom, moveTo);
+                view.enPassant(moveFrom, moveTo);
 
-moveHistory.push(model, moveFrom, moveTo);
-System.err.println(moveFrom + "-" + moveTo);
+                moveHistory.push(model, moveFrom, moveTo);
+                System.err.println(moveFrom + "-" + moveTo);
 
-} catch (SpecialMoveException e) {
-assert false;
-}
-}).start());
-	}
+            } catch (SpecialMoveException e) {
+                assert false;
+            }
+        }).start());
+    }
 
-	private static void updateChessboard() {
-		for (int i = 1; i < 9; ++i)
-			for (int j = 1; j < 9; ++j) {
-				Coordinates c = new Coordinates(i, j);
-				Piece piece = getModel().getPieceAt(c);
-				view.setIconAt(c, piece == null ? null : piece.toString());
-			}
-	}
+    private static void updateChessboard() {
+        for (int i = 1; i < 9; ++i)
+            for (int j = 1; j < 9; ++j) {
+                Coordinates c = new Coordinates(i, j);
+                Piece piece = getModel().getPieceAt(c);
+                view.setIconAt(c, piece == null ? null : piece.toString());
+            }
+    }
 
-	private static Game getModel() {
-		return model;
-	}
+    private static Game getModel() {
+        return model;
+    }
 
-	private static class SettingsHandler implements VetoableChangeListener, PropertyChangeListener {
+    private static class SettingsHandler implements VetoableChangeListener, PropertyChangeListener {
 
-		@Override
-		public void propertyChange(PropertyChangeEvent evt) {
-			if (evt.getPropertyName() == null)
-				return;
-			if (evt.getPropertyName().equals("gameTime")) {
-				String[] numbers = ((String) evt.getNewValue()).split(":");
-				model.setGameTime(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
-			} else if (evt.getPropertyName().equals("timeAdded")) {
-				model.setTimeAddedPerMove(Integer.parseInt((String) evt.getNewValue()));
-			} else
-				assert false;
-		}
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            if (evt.getPropertyName() == null)
+                return;
+            if (evt.getPropertyName().equals("gameTime")) {
+                String[] numbers = ((String) evt.getNewValue()).split(":");
+                model.setGameTime(Integer.parseInt(numbers[0]), Integer.parseInt(numbers[1]));
+            } else if (evt.getPropertyName().equals("timeAdded")) {
+                model.setTimeAddedPerMove(Integer.parseInt((String) evt.getNewValue()));
+            } else
+                assert false;
+        }
 
-		@Override
-		public void vetoableChange(PropertyChangeEvent evt) throws PropertyVetoException {
-			String newVal = ((String) evt.getNewValue()).trim();
-			if (evt.getPropertyName().equals("gameTime")) {
-				if (!newVal.matches("[0-9]+:[0-9][0-9]"))
-					throw new PropertyVetoException("Invalid format, use mm:ss", evt);
-				String[] numbers = newVal.split(":");
-				if (Integer.parseInt(numbers[1]) > 59)
-					throw new PropertyVetoException("Too many seconds!", evt);
-			} else if (evt.getPropertyName().equals("timeAdded")) {
-				if (!((String) evt.getNewValue()).matches("[0-9]+"))
-					throw new PropertyVetoException("invalid format", evt);
-			} else
-				assert false;
-		}
+        @Override
+        public void vetoableChange(PropertyChangeEvent evt) throws PropertyVetoException {
+            String newVal = ((String) evt.getNewValue()).trim();
+            if (evt.getPropertyName().equals("gameTime")) {
+                if (!newVal.matches("[0-9]+:[0-9][0-9]"))
+                    throw new PropertyVetoException("Invalid format, use mm:ss", evt);
+                String[] numbers = newVal.split(":");
+                if (Integer.parseInt(numbers[1]) > 59)
+                    throw new PropertyVetoException("Too many seconds!", evt);
+            } else if (evt.getPropertyName().equals("timeAdded")) {
+                if (!((String) evt.getNewValue()).matches("[0-9]+"))
+                    throw new PropertyVetoException("invalid format", evt);
+            } else
+                assert false;
+        }
 
-	}
+    }
 
 }
