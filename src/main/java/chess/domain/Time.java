085a6b8e96d6bdcfb94a98a2ffb81e82f8646740
Piotr <piotrub@gmail.com>
2016-10-15 13:23:38 +0200

    parsing Time from string

--- src/main/java/chess/domain/Time.java
+++ src/main/java/chess/domain/Time.java
@@ -1,5 +1,7 @@
 package chess.domain;
 
+import org.springframework.util.StringUtils;
+
 import java.io.Serializable;
 import java.time.Duration;
 
@@ -24,6 +26,36 @@ public class Time implements Serializable, Cloneable {
         this.timeRemaining = duration;
     }
     
+    private Time(int hours, int minutes, int seconds) {
+        this(60 * hours + minutes, seconds);
+    }
+    
+    public static Time fromString(String s) {
+        if (StringUtils.isEmpty(s)) {
+            throw new NumberFormatException("Empty string");
+        }
+        
+        int[] timeComponents = {0, 0, 0};
+        
+        String[] numbers = s.replaceAll("\\s+", "").split(":");
+        int len = numbers.length;
+        if (len > 3) {
+            throw new NumberFormatException("Invalid time format. Use at most three numbers separated with colon");
+        }
+        
+        for (int i = 3 - len; i < 3; ++i) {
+            timeComponents[i] = Integer.parseInt(numbers[i + len - 3]);
+            if (timeComponents[i] < 0) {
+                throw new NumberFormatException("Values must be non-negative");
+            }
+            if (i > 0 && timeComponents[i] > 60) {
+                throw new NumberFormatException("Number of minutes and seconds must be at most 60");
+            }
+        }
+        
+        return new Time(timeComponents[0], timeComponents[1], timeComponents[2]);
+    }
+    
     public void add(Time timeAdded) {
         timeRemaining = timeRemaining.plus(timeAdded.timeRemaining);
     }
@@ -34,18 +66,25 @@ public class Time implements Serializable, Cloneable {
     
     @Override
     public String toString() {
+        int hours = (int) timeRemaining.toHours();
+        int minutes = (int) timeRemaining.toMinutes() % 60;
+        int seconds = (int) (timeRemaining.getSeconds() % 60);
         StringBuilder sb = new StringBuilder();
-        int minutes = (int) timeRemaining.toMinutes();
+        
+        sb.append(hours);
+        sb.append(':');
+        
         if (minutes < 10) {
             sb.append('0');
         }
         sb.append(minutes);
         sb.append(':');
-        int seconds = (int) (timeRemaining.getSeconds() % 60);
+        
         if (seconds < 10) {
             sb.append('0');
         }
         sb.append(seconds);
+        
         return sb.toString();
     }
     
